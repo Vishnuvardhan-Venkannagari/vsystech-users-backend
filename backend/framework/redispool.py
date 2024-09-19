63f1d4c728683010a523b1357a070c2f216f3deb
Vishnu Reddy <vishnureddy@Vishnus-MacBook-Air.local>
2024-09-18 22:42:25 -0500

    changes in requirements

--- backend/framework/redispool.py
+++ backend/framework/redispool.py
@@ -1,8 +1,15 @@
-import redis
-import aioredis
+# import redis
+# import aioredis
 
-async def get_redis_connection() -> aioredis.Redis:
+# async def get_redis_connection() -> aioredis.Redis:
+#     '''
+#     Create a redis connection
+#     '''
+#     return await aioredis.from_url("redis://localhost:6379", encoding='utf8')
+import redis.asyncio as redis
+
+async def get_redis_connection() -> redis.Redis:
     '''
-    Create a redis connection
+    Create a redis connection using the async redis-py
     '''
-    return await aioredis.from_url("redis://localhost:6379", encoding='utf8')
+    return await redis.from_url("redis://localhost:6379", encoding='utf-8')
