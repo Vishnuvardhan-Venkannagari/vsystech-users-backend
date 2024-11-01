aa184664dd0d7e6f15b6dfeec61c0b3f9214bb7e
Vishnu Reddy <vishnureddy@Vishnus-MacBook-Air.local>
2024-10-31 19:03:08 -0500

    changes in get user

--- backend/application/users_getUser.py
+++ backend/application/users_getUser.py
@@ -20,7 +20,7 @@ async def get(id: str):
     get_user = restapi.db.child("users").child(id).get()
     user_data = dict(get_user.val())
     if not user_data.get("photo_url", ""):
-        user_data["photo_url"] = "https://media.istockphoto.com/id/1495088043/vector/user-profile-icon-avatar-or-person-icon-profile-picture-portrait-symbol-default-portrait.jpg?s=612x612&w=0&k=20&c=dhV2p1JwmloBTOaGAtaA3AW1KSnjsdMt7-U_3EZElZ0="
+        user_data["photo_url"] = "https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"
 
     return user_data
     # return await Use.get(id)
