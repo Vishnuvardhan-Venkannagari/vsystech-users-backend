64dceec40ec735c41f3becc04707101265e726e7
Vishnu Reddy <vishnureddy@Vishnus-MacBook-Air.local>
2024-11-17 18:47:25 -0600

    changes in oayment verify

--- backend/application/payements_verifyPayment.py
+++ backend/application/payements_verifyPayment.py
@@ -23,7 +23,7 @@ async def verifyPayment(token: str = fastapi.Query(...), PayerID: str = fastapi.
     query = {"order_id": token}
     order_details = await Payments.get_all(QueryParams(q=json.dumps(query), limit=10000))
     print(order_details)
-    data = {"gateway_name": "Paypal", "order_id": token}
+    data = {"gateway_name": "PayPal", "order_id": token}
     verify_order = await gateway().verifyOrder(data)
     print(verify_order)
     data = {"status": "Success", "msg": "Success"}
