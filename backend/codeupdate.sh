421bf2b3f15c933efa7b44c31bc2cbffe89b9c39
Vishnu Reddy <vishnureddy@Vishnus-MacBook-Air.local>
2024-11-17 21:52:26 -0600

    codeupdate

--- backend/codeupdate.sh
+++ backend/codeupdate.sh
@@ -16,6 +16,9 @@ prepareforUpdate() {
     if [ ! -d /opt/backend/application ]; then
           mkdir -p /opt/backend/application
     fi
+    if [ ! -d /opt/backend/websocket ]; then
+          mkdir -p /opt/backend/websocket
+    fi
 }
 
 updateCode() {
@@ -25,6 +28,7 @@ updateCode() {
     fi
     rsync -aSP $BASEDIR/framework/ /opt/backend/framework/ --delete
     rsync -aSP $BASEDIR/application/ /opt/backend/application/ --delete --exclude .env
+    rsync -aSP $BASEDIR/websocket/ /opt/backend/websocket/ --delete --exclude .env
     # rsync -aSP $BASEDIR/opt/socialswag/adminportal/ /opt/socialswag/adminportal/ --delete --exclude .env
    
     # if [ ! -f /opt/socialswag/integrations/.env ]; then
